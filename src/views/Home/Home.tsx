eee2a5ad96b1f3b3b43d41e0773f437caaedc5bb
Ralph Feltis <rfeltis@gmail.com>
2021-04-25 15:16:42 -0700

    Renames

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -83,9 +83,9 @@ const Home: React.FC = () => {
     <Page>
       <Hero>
         <Heading as="h1" size="xl" mb="24px" color="secondary">
-          {TranslateString(576, 'PancakeSwap')}
+          {TranslateString(576, 'RootSwap')}
         </Heading>
-        <Text>{TranslateString(578, 'The #1 AMM and yield farm on Binance Smart Chain.')}</Text>
+        <Text>{TranslateString(578, 'The #1 AMM and yield farm on Rootstock.')}</Text>
       </Hero>
       <div>
         <Cards>
